cfa61501068f0f70c542ca309075604907142ac3
Sampad Mahanty <176602095+Sampad6701@users.noreply.github.com>
2025-10-28 20:04:53 +0530

    Update and rename Q_05.c to Q_05_find_mistake.c

---
+++ 01_College_Work/02_Assignments/02_Minor_Assignment/Q_05_find_mistake.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    while ()
+    {
+        printf("%d", i++);
+        if (i > 10)
+            break;
+    }
+    return 0;
+}
+
+/*
+while loop has no condition
+error: expected expression before ‘)’ token
+*/
